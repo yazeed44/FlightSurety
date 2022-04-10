35334bfcf07273743ecc65334384eb408e8054d0
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2022-04-10 20:05:51 +0000

    Update compiler and ganache port

--- truffle.js
+++ truffle.js
@@ -1,19 +1,21 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      websockets: true,
+      // gas: 6721975,
+      // gasPrice: 20000000000
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.8.14"
     }
   }
 };
